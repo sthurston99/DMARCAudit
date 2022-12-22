48f25e3f6978e6e52a6c9260dd0fd68d1d197e0e
Simon Thurston <simongethurston@gmail.com>
2022-12-22 10:12:45 -0500

    initial commit

---
+++ ScanDmarc.ps1
@@ -0,0 +1,21 @@
+Param(
+    [Parameter(Mandatory)]
+    [string]$F
+)
+
+Import-Module BurntToast
+$temp = "C:\Temp\DMARC\" + $F
+$fn = ($F | Select-String -Pattern '.*(?=.xml|.zip|.gz)').Matches[0].Value
+$fn = ($fn | Select-String -Pattern '.*(?=.xml|.zip|.gz)').Matches[0].Value
+$path = "C:\Users\Simon Thurston\Documents\DMARC\" + $fn + ".xml"
+$domain = ($F | Select-String -Pattern '(?<=!)[\D\.]*(?=!)').Matches[0].Value
+$service = ($F | Select-String -Pattern '^.*?(?=!)').Matches[0].Value
+
+7z.exe x -o'C:\Users\Simon Thurston\Documents\DMARC\' $temp
+
+$fails = Select-String -path $path -pattern fail | % {$_.LineNumber}
+
+If ($fails -ne $null)
+{
+    New-BurntToastNotification -Text "DMARC Error for $domain","$service Detected a DMARC Failure on $domain on the following lines: $fails"
+}
