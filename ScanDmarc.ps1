6940d0e2001e1ad42df3d0bc3c137a1fae78fa05
Simon Thurston <44074551+sthurston99@users.noreply.github.com>
2023-01-09 16:18:26 -0500

    included ID in notification (#11)

--- ScanDmarc.ps1
+++ ScanDmarc.ps1
@@ -12,6 +12,7 @@ $fn = ($fn | Select-String -Pattern '.*(?=.xml|.zip|.gz)').Matches[0].Value
 $path = "" + $out + $fn + ".xml"
 $domain = ($F | Select-String -Pattern '(?<=!)[\D\.]*(?=!)').Matches[0].Value
 $service = ($F | Select-String -Pattern '^.*?(?=!)').Matches[0].Value
+$ID = ($F | Select-String -Pattern '(?<=!)\d*(?=\..{2,3}$)').Matches[0].Value
 
 Expand-7Zip -ArchiveFileName $temp -TargetPath $out
 
@@ -19,5 +20,5 @@ $fails = Select-String -path $path -pattern "(?<!soft)fail" | % {$_.LineNumber}
 
 If ($fails -ne $null)
 {
-    New-BTContentBuilder | Add-BTText "DMARC Error for $domain","$service Detected a DMARC Failure on $domain on the following lines: $fails" -PassThru | Show-BTNotification
+    New-BTContentBuilder | Add-BTText "DMARC Error $ID for $domain","$service Detected a DMARC Failure on $domain on the following lines: $fails" -PassThru | Show-BTNotification
 }
