46b0d70f4ed5fb54220acb0d765afafd816bc665
Simon Thurston <simongethurston@gmail.com>
2023-01-19 11:43:51 -0500

    removed messy debug

--- ScanDmarc.ps1
+++ ScanDmarc.ps1
@@ -13,14 +13,14 @@ $path = "" + $out + $fn + ".xml"
 $domain = ($F | Select-String -Pattern '(?<=!)[\D\.]*(?=!)').Matches[0].Value
 $service = ($F | Select-String -Pattern '^.*?(?=!)').Matches[0].Value
 $ID = ($F | Select-String -Pattern '(?<=!)\d*(?=\..{2,3}$)').Matches[0].Value
-New-BTContentBuilder | Add-BTText "here","line 16" -PassThru | Show-BTNotification
+
 If ($F -like "*.tar.gz*")
 {
     Expand-7Zip -ArchiveFileName $temp -TargetPath "C:\Temp\DMARC\"
     $temp = $temp.SubString(0,($temp.Length - 3))
 }
 Expand-7Zip -ArchiveFileName $temp -TargetPath $out
-New-BTContentBuilder | Add-BTText "here","line 23" -PassThru | Show-BTNotification
+
 $fails = (Select-String -path $path -pattern "(?<!soft)fail").count
 
 If ($fails -gt 0)
