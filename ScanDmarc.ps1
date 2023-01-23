864edc4258790deda914cd65761a7c20126e808b
Simon Thurston <simongethurston@gmail.com>
2023-01-23 16:49:29 -0500

    removed dependancy on 7Zip4Powershell, hopefully fixing tar issues

--- ScanDmarc.ps1
+++ ScanDmarc.ps1
@@ -7,19 +7,15 @@ Import-Module BurntToast
 Import-Module 7Zip4Powershell
 $temp = "C:\Temp\DMARC\" + $F
 $out = "" + $HOME + "\Documents\DMARC\"
-$fn = ($F | Select-String -Pattern '.*(?=.xml|.zip|.gz|.tar.gz)').Matches[0].Value
-$fn = ($fn | Select-String -Pattern '.*(?=.xml|.zip|.gz|.tar.gz)').Matches[0].Value
+$fn = ($F | Select-String -Pattern '.*(?=.xml|.zip|.gz)').Matches[0].Value
+$fn = ($fn | Select-String -Pattern '.*(?=.xml|.zip|.gz)').Matches[0].Value
 $path = "" + $out + $fn + ".xml"
 $domain = ($F | Select-String -Pattern '(?<=!)[\D\.]*(?=!)').Matches[0].Value
 $service = ($F | Select-String -Pattern '^.*?(?=!)').Matches[0].Value
 $ID = ($F | Select-String -Pattern '(?<=!)\d*(?=\..{2,3}$)').Matches[0].Value
 
-If ($F -like "*.tar.gz*")
-{
-    Expand-7Zip -ArchiveFileName $temp -TargetPath "C:\Temp\DMARC\"
-    $temp = $temp.SubString(0,($temp.Length - 3))
-}
-Expand-7Zip -ArchiveFileName $temp -TargetPath $out
+$param = @('x', "-o$out", "$temp")
+7z @param
 
 $fails = (Select-String -path $path -pattern "(?<!soft)fail").count
 
