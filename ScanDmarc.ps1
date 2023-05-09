798493304bd3039ff4a8e54bd6597eaf974dec19
Simon Thurston <simongethurston@gmail.com>
2023-05-09 11:54:47 -0400

    switched fail thresh to environment variable; minor code formatting

--- ScanDmarc.ps1
+++ ScanDmarc.ps1
@@ -15,8 +15,8 @@ $param = @('x', "-o$out", "$temp")
 7z @param
 
 [xml]$xml = (Get-Content -Path $path)
-$dkimfails = ($xml.feedback.record.auth_results.dkim.result | Where-Object {$_ -eq "fail"} | Measure-Object).count
-$spffails = ($xml.feedback.record.auth_results.spf.result | Where-Object {$_ -eq "fail"} | Measure-Object).count
+$dkimfails = ($xml.feedback.record.auth_results.dkim.result | Where-Object { $_ -eq "fail" } | Measure-Object).count
+$spffails = ($xml.feedback.record.auth_results.spf.result | Where-Object { $_ -eq "fail" } | Measure-Object).count
 $fails = $dkimfails + $spffails
 
 $service = $xml.feedback.report_metadata.org_name
@@ -24,7 +24,7 @@ $domain = $xml.feedback.policy_published.domain
 
 If ($fails -gt 0) {
     Add-Content -Path "$out\log.txt" -Value "$ID : $service reporting $domain failed $fails times"
-    If ($fails -gt 10) {
+    If ($fails -gt $Env:FailThresh) {
         New-BTContentBuilder | Add-BTText "DMARC Error $ID for $domain", "$service Detected $fails DMARC Failure(s) on $domain" -PassThru | Show-BTNotification
     }
 } Else {
